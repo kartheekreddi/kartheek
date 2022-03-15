547b9d4f3fce60d4fc655b73206cf2abd0f0c1ca
Kartheek Reddy <95276806+kartheekreddi@users.noreply.github.com>
2022-03-15 14:05:27 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,13 @@
+class Vehicle{
+    constructor(){
+        console.log('This message is from constructor');
+    }
+
+    display(){
+        console.log('This is from method');
+    }
+
+}
+let vehicle = new Vehicle();
+//vehicle.display();
+  
